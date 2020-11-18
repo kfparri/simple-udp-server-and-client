695afb10a9eaef45e744cf784076052739f51417
Kyle Parrish <kfparri@nppd.com>
2020-11-18 06:30:04 -0600

    first commit

---
+++ udp_client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace udp_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the destination IP: ");
+            var ip = Console.ReadLine();
+
+            Console.Write("Port Number: ");
+            var port = Console.ReadLine();
+
+            UDPSocket c = new UDPSocket();
+            c.Client(ip, Convert.ToInt32(port));
+            c.Send("Client Started up!");
+
+            while(true)
+            {
+                var result = Console.ReadLine();
+                c.Send(result);
+            }
+
+            
+        }
+    }
+}
---
+++ udp_server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace udp_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Port to lisen on: ");
+            var port = Console.ReadLine();
+
+            UDPSocket s = new UDPSocket();
+            s.Server(Convert.ToInt32(port));
+            //s.Server("127.0.0.1", 27000);
+
+            Console.ReadKey();
+        }
+    }
+}
